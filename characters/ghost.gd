4b9e198c12869a7c6e235a29971150cbc93dbd2e
juliohq <JulioHenrique31501234@gmail.com>
2025-09-18 09:08:20 -0300

    Add ghost navigation

--- characters/ghost.gd
+++ characters/ghost.gd
@@ -6,8 +6,15 @@ const SPEED := 64
 
 func _physics_process(_delta: float) -> void:
 	var player = get_tree().get_first_node_in_group(&"players")
-	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * SPEED
+	
+	%Agent.target_position = player.global_position
+	var next_position: Vector2 = %Agent.get_next_path_position()
+	
+	if %Agent.is_navigation_finished():
+		velocity = Vector2()
+	else:
+		velocity = global_position.direction_to(next_position) * SPEED
+	
 	move_and_slide()
 
 
--- levels/level.gd
+++ levels/level.gd
@@ -2,8 +2,5 @@ extends Node2D
 
 
 func _ready() -> void:
-	# Spawn ghost
-	var ghost := preload("res://characters/ghost.tscn").instantiate()
 	var spawn_point: Node2D = %SpawnPoints.get_children().pick_random()
-	ghost.global_position = spawn_point.global_position
-	%World.add_child(ghost)
+	%Ghost.global_position = spawn_point.global_position
